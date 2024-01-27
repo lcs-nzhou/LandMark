877e9b7fc01d48730730b0e5fe575c32a46bc333
Nina Zhou <nzhou@lcs.on.ca>
2024-01-27 09:53:30 -0500

    Initial Commit

---
+++ LandMark/LandMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarkApp.swift
+//  LandMark
+//
+//  Created by Shuyu Zhou on 2024-01-27.
+//
+
+import SwiftUI
+
+@main
+struct LandMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
